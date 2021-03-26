26bafc68aeaf47b0cc4b5cfc00fd497cf75c51f3
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-03-26 10:20:14 +0100

    Added hyperparameters for edge detection

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from Defect import crack_defect as crack
 from Defect import pinhole_defct as pinhole
 from Preprocessing import pre_processing as preprocess
 import cv2 as cv
+import numpy as np
+import sys
 
-img = cv.imread("Resources/crackTile4.jpg")
+img = cv.imread("Resources/crack/crackTile5.jpg")
 cv.imshow("Img original", img)
+
+method = "Sobel"
 
 img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
 # Preprocessing
-img_pre_processing = preprocess.start(img_gray)
-# Pinhole defect
-pinhole.detect(img_pre_processing)
-# Crack defect
-# crack_detect = crack.detect(img_pre_processing)
+img_pre_processing = preprocess.start(img_gray, method=method)
 
-cv.imshow("Img result canny", img_pre_processing)
-# cv.imshow("Result Crack", crack_detect)
+# Pinhole defect
+# pinhole.detect(img_pre_processing)
+
+# Crack defect
+crack_detect = crack.detect(img_pre_processing, method=method)
+
+cv.imshow(f"Edge detection {method}", img_pre_processing)
+cv.imshow("Result Crack", crack_detect)
 cv.waitKey(0)
