80332f8c84bc4e30826e92e1cc11d27ff82466c9
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-04-06 18:46:11 +0200

    Initialized image to show

--- GUI/Images.py
+++ GUI/Images.py
@@ -1,18 +1,20 @@
-from tkinter import Frame, Label, Scrollbar, Listbox, RIGHT, X, Y
+from tkinter import Frame, Scrollbar, RIGHT, X
+from GUI.ScrollableImage import ScrollableImage
 
 class ImageEntry(Frame):
+    r"""
+    Class Images
+    """
     def __init__(self, parent):
         super().__init__(parent)
         self.pack(fill=X)
+        self.scrollbar = Scrollbar(self, orient='vertical')
 
-        self.scrollbar = Scrollbar(self)
-        self.scrollbar.pack(side=RIGHT, fill=Y)
-        self.list = Listbox(self, yscrollcommand=self.scrollbar.set)
-
-    def addImage(self, img):
-        panel = Label(self, image=img)
-        panel.image = img
-        panel.pack(side=RIGHT, padx=30)
-        self.list.insert(panel)
-        self.scrollbar.config(command=self.list.yview)
+    def add_image(self, img):
+        r"""
+        Showed the image in the GUi
+        :param img: image to show
+        """
+        image_window = ScrollableImage(self, image=img, scrollbarwidth=6, width=400, height=400)
+        image_window.pack(side=RIGHT, padx=10)
 
