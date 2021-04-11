8a16bf925f33aed84bfa5c4942b86395d1d8a890
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-04-11 11:33:44 +0200

    Moved the division of image for 255

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from Preprocessing import pre_processing as preprocess
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("Resources/crack/crackTile5.jpg")
+img = cv.imread("Resources/crack/crackTile.jpg")
 cv.imshow("Img original", img)
 
-method = "Sobel"
+method = "Canny"
 
 img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
@@ -18,7 +18,7 @@ img_pre_processing = preprocess.start(img_gray, method=method)
 # pinhole.detect(img_pre_processing)
 
 # Crack defect
-crack_detect = crack.detect(img, img_pre_processing, method=method)
+crack_detect = crack.detect(img, img_pre_processing / 255, method=method)
 
 cv.imshow(f"Edge detection {method}", img_pre_processing)
 cv.imshow("Result Crack", crack_detect)
