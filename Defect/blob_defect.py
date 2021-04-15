8b97f434638188fb299fa2ef78087f1e65453a77
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-04-15 11:25:30 +0200

    Added documetation

--- Defect/blob_defect.py
+++ Defect/blob_defect.py
@@ -2,16 +2,23 @@ import numpy as np
 import cv2 as cv
 
 
-def detect(original, img, method="Sobel"):
+def detect(img_original, img_edge, method="Sobel"):
+    r"""
+    Detects blobs in the image
+    :param img_original: original image in which to draw the defects
+    :param img_edge: image in which to detect blobs
+    :param method: edge detection method (canny, sobel)
+    :return:
+    """
     if method == "Sobel":
-        _, img = cv.threshold(img, 50, 255, cv.THRESH_BINARY)
+        _, img_edge = cv.threshold(img_edge, 50, 255, cv.THRESH_BINARY)
 
     element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-    img = cv.dilate(img, element, iterations=2)
-    img = cv.erode(img, element, iterations=1)
+    img_edge = cv.dilate(img_edge, element, iterations=2)
+    img_edge = cv.erode(img_edge, element, iterations=1)
 
-    #cv.imshow("After morphologic", img)
-    contours, hierarchy = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    # cv.imshow("After morphologic", img)
+    contours, hierarchy = cv.findContours(img_edge, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
     centers = []
     contours_to_draw = []
@@ -26,7 +33,6 @@ def detect(original, img, method="Sobel"):
         approx = cv.approxPolyDP(cnt, 0.05 * peri, True)
         objCor = len(approx)
         if objCor > 4:
-
             contours_to_draw.append(cnt)
             m = cv.moments(cnt)
             center = (int(m['m10'] / m['m00']), int(m['m01'] / m['m00']))
@@ -34,6 +40,6 @@ def detect(original, img, method="Sobel"):
 
     for cnt in contours_to_draw:
         # cv.circle(original, center, 3, (255, 0, 0), -1)
-        cv.drawContours(original, cnt, -1, (0, 0, 255), 3)
+        cv.drawContours(img_original, cnt, -1, (0, 0, 255), 3)
 
-    return original
+    return img_original
