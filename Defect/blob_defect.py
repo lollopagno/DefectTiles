d45d7819e75a442920a37ec344202f6519a29785
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-04-19 15:36:29 +0200

    Improved detection

--- Defect/blob_defect.py
+++ Defect/blob_defect.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2 as cv
+from Defect import common as utility
 
+SOBEL = "Sobel"
+BLOBS = "Blob"
 
-def detect(img_original, img_edge, method="Sobel"):
+def detect(img_original, img_edge, method=SOBEL):
     r"""
     Detects blobs in the image
     :param img_original: original image in which to draw the defects
@@ -10,36 +13,34 @@ def detect(img_original, img_edge, method="Sobel"):
     :param method: edge detection method (canny, sobel)
     :return:
     """
-    if method == "Sobel":
+
+    if method == SOBEL:
         _, img_edge = cv.threshold(img_edge, 50, 255, cv.THRESH_BINARY)
 
-    element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-    img_edge = cv.dilate(img_edge, element, iterations=2)
-    img_edge = cv.erode(img_edge, element, iterations=1)
+    kernel = np.ones((3, 35), np.uint8)
+    img_closing = cv.morphologyEx(img_edge, cv.MORPH_CLOSE, kernel)
+    cv.imshow("Closing", img_closing)
 
-    # cv.imshow("After morphologic", img)
-    contours, hierarchy = cv.findContours(img_edge, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    # element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+    # img_edge = cv.dilate(img_edge, element, iterations=2)
+    # img_ellipse = cv.erode(img_edge, element, iterations=1)
+    # cv.imshow("Ellipse", img_ellipse)
 
-    centers = []
-    contours_to_draw = []
+    contours, _ = cv.findContours(img_closing, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
     for cnt in contours:
 
         area = cv.contourArea(cnt)
-
-        if area < 10:
+        if area < 2.0:
             continue
 
         peri = cv.arcLength(cnt, True)
         approx = cv.approxPolyDP(cnt, 0.05 * peri, True)
         objCor = len(approx)
-        if objCor > 4:
-            contours_to_draw.append(cnt)
-            m = cv.moments(cnt)
-            center = (int(m['m10'] / m['m00']), int(m['m01'] / m['m00']))
-            centers.append(center)
 
-    for cnt in contours_to_draw:
-        # cv.circle(original, center, 3, (255, 0, 0), -1)
-        cv.drawContours(img_original, cnt, -1, (0, 0, 255), 3)
+        if 4 <= objCor <= 7: # TODO cehck this!
+
+            if utility.calc_distance(cnt, BLOBS):
+                cv.drawContours(img_original, cnt, -1, (0, 0, 255), 3)
 
     return img_original
