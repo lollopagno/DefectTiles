aee91282ac37242a5042c9adabb9c5164ff415da
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-05-08 09:22:22 +0200

    Postponed morphologic operations after searching for connected components

--- Defect/blob_defect.py
+++ Defect/blob_defect.py
@@ -16,14 +16,7 @@ def detect(img_original, img_edge):
     :return:
     """
 
-    # Kernel
-    kernel_5 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
-    kernel_3 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-
-    dilate = cv.dilate(img_edge, kernel_3)
-    img_closing = cv.morphologyEx(dilate, cv.MORPH_CLOSE, kernel_5)
-
-    blobs = utility.connected_components(img_closing / 255)
+    blobs = utility.connected_components(img_edge / 255)
     blobs_detect = np.zeros(img_edge.shape[:2], dtype=np.float64)
 
     mean_original_img = cv.mean(img_original)[0]
@@ -33,6 +26,7 @@ def detect(img_original, img_edge):
 
             intensity_pixels = []
             contours_blob = []
+
             for _ in range(0, len(blob)):
                 x, y = blob.pop()
                 contours_blob.append([y, x])
@@ -40,12 +34,17 @@ def detect(img_original, img_edge):
 
             avg_luminance_area_contour = np.mean(intensity_pixels)
 
-            print(avg_luminance_area_contour, mean_original_img)  # TODO capire cosa non va qui
-            if avg_luminance_area_contour - 10 < mean_original_img:
+            if avg_luminance_area_contour < mean_original_img:
                 contours_blob = np.array(contours_blob).astype(np.int32)
                 cv.drawContours(blobs_detect, [contours_blob], -1, WHITE, -1)
 
-        contours_blob, _ = cv.findContours(blobs_detect.copy().astype(np.uint8), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        # Kernel
+        kernel_5 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+        kernel_3 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+
+        dilate = cv.dilate(blobs_detect, kernel_3)
+        img_closing = cv.morphologyEx(dilate, cv.MORPH_CLOSE, kernel_5)
+        contours_blob, _ = cv.findContours(img_closing.astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         blobs_detect = np.zeros((img_original.shape[:2]), dtype=np.uint8)
 
         # Detect blobs
