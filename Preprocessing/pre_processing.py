e03f858b9cb0c8ef68fa42eb0ecb18ed70669f7c
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-05-12 12:31:27 +0200

    Delete TODO

--- Preprocessing/pre_processing.py
+++ Preprocessing/pre_processing.py
@@ -31,7 +31,7 @@ def start(img_original, filter):
         img_norm = cv.normalize(img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
     else:
         img_norm = cv.normalize(img, None, alpha=0, beta=170,
-                                norm_type=cv.NORM_MINMAX)  # TODO Variare beta: prima era a 120
+                                norm_type=cv.NORM_MINMAX)
 
     # Applying the filter (noise reduction)
     # Median
