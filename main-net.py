e6680f1ce4ed3506affd37cdc25daeb292b85105
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-05-24 14:39:15 +0200

    Initialized main u-net

--- main-net.py
+++ main-net.py
@@ -1,6 +1,24 @@
-from UNet import unet as Net
+from UNet.unet import Unet
+import torch
+from torchsummary import summary
 from torch.utils.tensorboard import SummaryWriter
 
-net = Net.Unet()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-writer = SummaryWriter('experiment_unet_1')
+model = Unet().to(device)
+
+total_params = sum(p.numel() for p in model.parameters())
+trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f"Total parameters: {total_params}")
+print(f"Trainable parameters: {trainable_params}")
+print(f"Non-trainable params: {(total_params - trainable_params)}")
+print("********************************\n\n")
+
+# writer = SummaryWriter("runs/unet_experiment_1")
+# x = torch.randn(1, 3, 224, 224)
+# writer.add_graph(model, x)
+# writer.close()
+
+summary(model, (1, 512, 512))
+
