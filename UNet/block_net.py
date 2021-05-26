e7017a7dccceb98e9e7c5b5dbc3e7c63222f8661
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-05-26 11:44:46 +0200

    Completed the up block. Added documentation

--- UNet/block_net.py
+++ UNet/block_net.py
@@ -9,24 +9,31 @@ from UNet.utility import get_convolutional_layers, \
 
 class Down(nn.Module):
     r"""
-    # TODO documentation
+    Downsampling block.
     """
 
     def __init__(self,
                  in_channel: int,
                  out_channel: int):
+        r"""
+        :param in_channel: number of input filters.
+        :param out_channel: number of output filters.
+        """
         super().__init__()
 
-        self.down_block_1 = get_convolutional_layers(in_channel, out_channel)
-        self.down_block_2 = get_convolutional_layers(out_channel, out_channel)
+        self.conv_1 = get_convolutional_layers(in_channel, out_channel)
+        self.conv_2 = get_convolutional_layers(out_channel, out_channel)
         self.activation = get_relu()
         self.max_pooling = get_max_pooling()
 
     def forward(self, x):
-        out_conv = self.down_block_1(x)
+        r"""
+        Forward down block.
+        """
+        out_conv = self.conv_1(x)
         out_relu = self.activation(out_conv)
 
-        out_conv = self.down_block_2(out_relu)
+        out_conv = self.conv_2(out_relu)
         out_relu = self.activation(out_conv)
 
         out_pooling = self.max_pooling(out_relu)
@@ -35,23 +42,31 @@ class Down(nn.Module):
 
 class Bottleneck(nn.Module):
     r"""
-    # TODO documentation
+    Bottleneck block.
     """
 
     def __init__(self,
                  in_channel: int,
                  out_channel: int):
+        r"""
+
+        :param in_channel: number of input filters.
+        :param out_channel: number of output filters.
+        """
         super().__init__()
 
-        self.bottleneck_block_1 = get_convolutional_layers(in_channel, out_channel)
-        self.bottleneck_block_2 = get_convolutional_layers(out_channel, out_channel)
+        self.conv_1 = get_convolutional_layers(in_channel, out_channel)
+        self.conv_2 = get_convolutional_layers(out_channel, out_channel)
         self.activation = get_relu()
 
     def forward(self, x):
-        out_conv = self.bottleneck_block_1(x)
+        r"""
+        Forward bottleneck block.
+        """
+        out_conv = self.conv_1(x)
         out_relu = self.activation(out_conv)
 
-        out_conv = self.bottleneck_block_2(out_relu)
+        out_conv = self.conv_2(out_relu)
         out_relu = self.activation(out_conv)
 
         return out_relu
@@ -59,52 +74,78 @@ class Bottleneck(nn.Module):
 
 class Up(nn.Module):
     r"""
-    # TODO documentation
+    Upsampling block.
     """
 
     def __init__(self,
                  in_channel: int,
                  out_channel: int):
+        r"""
+        :param in_channel: number of input filters.
+        :param out_channel: number of output filters.
+        """
         super().__init__()
+
         self.up_sample = get_up_sample()
         self.concat = Concat()
+        self.activation = get_relu()
+        self.conv_1 = get_convolutional_layers(in_channel=in_channel, out_channel=out_channel)
+        self.conv_2 = get_convolutional_layers(out_channel, out_channel)
 
     def forward(self, x, skip):
-        print(f"After upsample: {x.shape}")
+        r"""
+        Forward upblock.
+        :param x: input layer.
+        :param skip: skip connection layer to concatenate.
+        """
         out_up = self.up_sample(x)
-        print(f"Concat out upsample: {out_up.shape}")
-        print(f"Concat skip: {skip.shape}")
-        self.concat(out_up, skip)
+        out_concat = self.concat(out_up, skip)
+        out_conv = self.conv_1(out_concat)
+        out_relu = self.activation(out_conv)
+        out_conv = self.conv_2(out_relu)
+        out_relu = self.activation(out_conv)
+
+        return out_relu
 
 
 class Concat(nn.Module):
     r"""
-    # TODO documentation
+    Concatenate class.
+    Concatenate two levels of the network.
     """
 
     def __init__(self):
         super().__init__()
 
     def forward(self, layer_1, layer_2):
-        out_cat = torch.cat((layer_1, layer_2), dim=1)
-        return out_cat
+        out_concat = torch.cat((layer_1, layer_2), dim=1)
+        return out_concat
 
 
 class Out(nn.Module):
     r"""
-    # TODO documentation
+    Output block.
     """
 
     def __init__(self,
                  in_channel: int,
                  out_channel: int,
                  kernel_size: tuple):
+        r"""
+        :param in_channel: number of input filters.
+        :param out_channel: number of output filters.
+        :param kernel_size: kernel size.
+        """
         super().__init__()
 
-        self.conv = get_convolutional_layers(in_channel=in_channel, out_channel=out_channel, kernel_size=kernel_size)
+        self.conv = get_convolutional_layers(in_channel=in_channel, out_channel=out_channel, kernel_size=kernel_size,
+                                             padding=(0, 0))
         self.sigmoid = get_sigmoid()
 
     def forward(self, x: torch.Tensor):
+        r"""
+        Forward output block.
+        """
         out_conv = self.conv(x)
         out_sigmoid = self.sigmoid(out_conv)
 
