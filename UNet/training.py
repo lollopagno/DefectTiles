48ce7e576013ff6ebedb3a1e749a9c2a40c280be
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-01 18:47:31 +0200

    Added training on test data, early stopping, saving the model, other improvement

--- UNet/training.py
+++ UNet/training.py
@@ -1,19 +1,25 @@
 import torch
 import numpy as np
-from tqdm import tqdm
 from UNet.metric import accuracy
+from UNet.earlyStopping import EarlyStopping
+import datetime
+import os
+
+SHOW_EVERY = 1
+PARENT_DIR = "UNet/ModelSaved"
 
 
-def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_loader, validation_loader):
+def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_loader, validation_loader, test_loader):
     r"""
-    # TODO documentation
-    :param model
-    :param num_epochs
-    :param optimizer
-    :param lr_scheduler
-    :param loss_fn
-    :param training_loader
-    :param validation_loader
+    Network training
+    :param model: neural network model
+    :param num_epochs: number of epochs
+    :param optimizer: optimize used
+    :param lr_scheduler: scheduler used
+    :param loss_fn: loss function used
+    :param training_loader: training data
+    :param validation_loader: validation data
+    :param test_loader: test data
     """
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,13 +27,22 @@ def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_
     # Initialized params before training
     training_loss_arr = []
     validation_loss_arr = []
+    test_loss_arr = []
 
     training_accuracy_arr = []
     validation_accuracy_arr = []
+    test_accuracy_arr = []
 
-    epoch_count = 0
+    # Parameters to plot loss and accuracy
+    plot_train_loss = np.zeros(num_epochs)
+    plot_validate_loss = np.zeros(num_epochs)
+    plot_validate_accuracy = np.zeros(num_epochs)
 
-    for _ in tqdm(range(0, num_epochs)):
+    # Early stopping
+    early_stopping = EarlyStopping()
+
+    for epoch in range(0, num_epochs):
+        print(f"Epoch: {epoch + 1}/{num_epochs}")
 
         lr_scheduler.step()
 
@@ -44,7 +59,8 @@ def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_
 
             # Split the train data from the labels
             X = batch[0].to(device)
-            y = batch[1].to(device)  # TODO batch_label[batch_label>0] = 1
+            y = batch[1].to(device)
+            y[y > 0] = 1  # TODO check this
 
             # Forward pass
             y_predicted = model(X)
@@ -65,40 +81,88 @@ def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_
         training_loss_arr.append(np.divide(training_loss_batch, num_steps))
         training_accuracy_arr.append(np.divide(training_accuracy_batch, num_steps))
 
-        # TODO check print
-        print(f"** Training\nEpoch: {epoch_count + 1}\nLoss: {np.mean(training_loss_arr)}\n"
-              f"Accuracy: {np.mean(training_accuracy_arr)}\n**\n\n")
+        # Set loss (training) for each epoch
+        plot_train_loss[epoch] = training_loss_arr
 
-        # Validation steps
+        # Validation
         with torch.no_grad():
 
             model.eval()
 
-            validation_loss_batch = 0.0
-            validation_accuracy_batch = 0.0
-            num_steps = 0
-
-            for index, batch in enumerate(validation_loader):
-                torch.cuda.empty_cache()
-
-                X = batch[0].to(device)
-                y = batch[1].to(device)  # TODO batch_label[batch_label>0] = 1
-
-                y_predicted = model(X)
-                loss = loss_fn(y_predicted, y)
-                validation_loss_batch += loss.item()
-                validation_accuracy_batch += accuracy(y_predicted.cpu().detach().numpy(),
-                                                      y.cpu().detach().numpy())
-
-                num_steps += 1
+            # Validation steps
+            validation_loss_batch, validation_accuracy_batch, num_steps = _training_loop(validation_loader, model,
+                                                                                         loss_fn, device)
 
             validation_loss_arr.append(np.divide(validation_loss_batch, num_steps))
             validation_accuracy_arr.append(np.divide(validation_accuracy_batch, num_steps))
-            print(f"** Validation\nEpoch: {epoch_count + 1}\nLoss: {np.mean(validation_loss_arr)}\n"
-                  f"Accuracy: {np.mean(validation_accuracy_arr)}\n**\n\n")
 
-        epoch_count += 1
+            # Set loss and accuracy (validation) for each epoch
+            plot_validate_loss[epoch] = validation_loss_arr
+            plot_validate_accuracy[epoch] = validation_accuracy_arr
 
-    # TODO added training test
+        if epoch % SHOW_EVERY == 0 or epoch == num_epochs - 1:
+            print(f"** Training\n\t\tLoss: {np.round(np.mean(validation_loss_arr), 4)}\n\t\t"
+                  f"Accuracy: {np.round(np.mean(validation_accuracy_arr), 4)}\n**\n\n")
 
-    return training_loss_arr, validation_loss_arr, training_accuracy_arr, validation_accuracy_arr
+            print(f"** Validation\n\t\tLoss: {np.round(np.mean(training_loss_arr), 4)}\n\t\t"
+                  f"Accuracy: {np.round(np.mean(training_accuracy_arr), 4)}\n**\n\n")
+
+        early_stopping(validation_loss_arr)
+        if early_stopping.early_stop:
+            # Save the model
+            current_date_hour = datetime.datetime.now()
+            new_dir = f"{current_date_hour.year}{current_date_hour.month}{current_date_hour.day}-" \
+                      f"{current_date_hour.hour}{current_date_hour.minute}{current_date_hour.second} "
+            path = os.path.join(PARENT_DIR, new_dir)
+            os.mkdir(path)
+
+            torch.save(model.state_dict(), path + "/")
+            print("Model saved!")
+
+            # Stop training
+            break
+
+    # Test
+    with torch.no_grad():
+
+        model.eval()
+
+        # Test steps
+        test_loss_batch, test_accuracy_batch, num_steps = _training_loop(test_loader, model, loss_fn, device)
+
+        test_loss_arr.append(np.divide(test_loss_batch, num_steps))
+        test_accuracy_arr.append(np.divide(test_accuracy_batch, num_steps))
+        print(f"** Test\n\t\tLoss: {np.round(np.mean(test_loss_arr), 4)}\n\t\t"
+              f"Accuracy: {np.round(np.mean(test_accuracy_arr), 4)}\n**\n\n")
+
+    return plot_train_loss, plot_validate_loss, plot_validate_accuracy
+
+
+def _training_loop(data_loader, model, loss_fn, device):
+    r"""
+    :param data_loader: data to be input to the network
+    :param model: neural network model
+    :param loss_fn: loss function used
+    :param: cpu or gpu
+    """
+
+    total_loss = 0.0
+    total_accuracy = 0.0
+    num_steps = 0
+
+    for index, batch in enumerate(data_loader):
+        torch.cuda.empty_cache()
+
+        X = batch[0].to(device)
+        y = batch[1].to(device)
+        y[y > 0] = 1  # TODO check this
+
+        y_predicted = model(X)
+        total_loss = loss_fn(y_predicted, y)
+        total_loss += total_loss.item()
+
+        total_accuracy += accuracy(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
+
+        num_steps += 1
+
+    return total_loss, total_accuracy, num_steps
