835cf19b95515dcf085ace94fbdef1f0b5d57b61
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-04 16:46:35 +0200

    Added function to plot test results. Improved function to plot samples of the dataset.

--- UNet/plot.py
+++ UNet/plot.py
@@ -15,7 +15,7 @@ def plot_history(loss_train, loss_valid, accuracy_valid, num_epochs):
     plot_epoch = np.arange(0, num_epochs)
 
     plt.figure(1)
-    plt.plot(plot_epoch[0:num_epochs], loss_train[0:num_epochs], linestyle='--', marker='',
+    plt.plot(plot_epoch[0:num_epochs], loss_train[0:num_epochs], linestyle='-', marker='',
              linewidth=3, alpha=0.9)
     plt.plot(plot_epoch[0:num_epochs], loss_valid[0:num_epochs], linestyle='-', marker='',
              linewidth=3, alpha=0.9)
@@ -30,7 +30,7 @@ def plot_history(loss_train, loss_valid, accuracy_valid, num_epochs):
              linewidth=3, alpha=0.9)
     plt.xlabel('Epoch number')
     plt.ylabel('Accuracy')
-    plt.gca().legend('Accuracy', loc='lower right')
+    plt.gca().legend('Accuracy', loc='upper right')
     plt.title('Accuracy history')
     plt.show()
 
@@ -39,7 +39,7 @@ def sample_dataset(data_loader, batch_size):
     r"""
     Shows an example of the training dataset
     :param data_loader: dataset
-    :param batch_size: number of batchsize
+    :param batch_size: number of batch size
     """
 
     counter = 0
@@ -74,3 +74,36 @@ def plot_samples(index, img, label):
     plt.title('Label')
     plt.imshow(label, cmap="gray")
     plt.show()
+
+
+def plot_test_results(images, masks, predicted, value):
+    r"""
+    PLot test results.
+    :param images: collection of images of the dataset.
+    :param masks: collection of masks of the dataset.
+    :param predicted: images predicted by the network.
+    :param value: initial value of loop.
+    """
+
+    if value >= len(images):
+        raise Exception("The value must be less than the number of images")
+
+    for i in range(value, len(images)):
+        img = images[i]
+        mask = masks[i]
+        predict = predicted[i]
+
+        # Image
+        plt.title('Image')
+        plt.imshow(img, cmap="gray")
+        plt.show()
+
+        # Label
+        plt.title('Mask')
+        plt.imshow(mask, cmap="gray")
+        plt.show()
+
+        # Predict
+        plt.title('Predict')
+        plt.imshow(predict, cmap="gray")
+        plt.show()
