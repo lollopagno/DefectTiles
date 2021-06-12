7b87cb2627c09b5aa9245fa012e1a3b711723c8b
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-12 13:00:30 +0200

    Fix training loop

--- UNet/training.py
+++ UNet/training.py
@@ -41,132 +41,119 @@ def training_loop(model, num_epochs, optimizer, lr_scheduler, loss_fn, training_
     validation_IoU_arr = []
 
     # Parameters to plot loss and accuracy
-    plot_train_loss = np.zeros(num_epochs)
-    plot_validate_loss = np.zeros(num_epochs)
-    plot_validate_accuracy = np.zeros(num_epochs)
-    plot_validate_IoU = np.zeros(num_epochs)
+    plot_train_loss = []
+    plot_validate_loss = []
+    plot_validate_accuracy = []
+    plot_validate_IoU = []
 
     # Early stopping
     early_stopping = EarlyStopping()
 
     for epoch in range(0, num_epochs):
-        print(f"--> Epoch: {epoch + 1}/{num_epochs}")
 
-        # Training steps
-        training_loss_batch = 0.0
-        training_accuracy_batch = 0.0
-        training_IoU_batch = 0.0
-        num_steps = 0
+        # Training loop
 
-        bar = tqdm(training_loader, total=len(training_loader))
-        for X, y in bar:
-            torch.cuda.empty_cache()
+        training_loss_batch = []
+        training_accuracy_batch = []
+        training_IoU_batch = []
 
-            # Training model
-            model.train()
-
+        train_bar = tqdm(training_loader, total=len(training_loader))
+        for X, y in train_bar:
             # Split the train data from the labels
             X = X.to(device)
             y = y.to(device)
             y[y > 0] = 1
 
+            optimizer.zero_grad()
+
             # Forward pass
             y_predicted = model(X)
 
-            # if num_steps == 1:
-            #     import matplotlib.pyplot as plt
-            #     arr_ = np.squeeze(X.detach().cpu().numpy())
-            #     arr_ = arr_[0, 0, :, :]
-            #     plt.imshow(arr_)
-            #     plt.show()
-            #
-            #     arr_ = np.squeeze(y.detach().cpu().numpy())
-            #     print(f"Mask: {arr_.shape}")
-            #     arr_ = arr_[0, :, :]
-            #     plt.imshow(arr_)
-            #     plt.show()
-            #
-            #     arr_ = np.squeeze(y_predicted.detach().cpu().numpy())
-            #     print(f"Predict: {arr_.shape}")
-            #     arr_ = arr_[0, :, :]
-            #     plt.imshow(arr_)
-            #     plt.show()
-
             loss = loss_fn(y_predicted, y)
+            loss_value = loss.item()
 
-            optimizer.zero_grad()
+            accuracy_value = accuracy(y_predicted.cpu().detach().numpy(),
+                                      y.cpu().detach().numpy())
+
+            IoU_value = IoU(y_predicted.cpu().detach().numpy(),
+                            y.cpu().detach().numpy())
 
             # Backward pass
             loss.backward()
             optimizer.step()
 
-            training_loss_batch += loss.item()
-            training_accuracy_batch += accuracy(y_predicted.cpu().detach().numpy(),
-                                                y.cpu().detach().numpy())
+            # Added Loss
+            training_loss_batch.append(loss_value)
 
-            training_IoU_batch += IoU(y_predicted.cpu().detach().numpy(),
-                                      y.cpu().detach().numpy())
+            # Added accuracy
+            training_accuracy_batch.append(accuracy_value)
 
-            num_steps += 1
+            # Added IoU
+            training_IoU_batch.append(IoU_value)
 
-        train_loss_for_this_epoch = np.divide(training_loss_batch, num_steps)
-        training_loss_arr.append(train_loss_for_this_epoch)
-        training_accuracy_arr.append(np.divide(training_accuracy_batch, num_steps))
-        training_IoU_arr.append(np.divide(training_IoU_batch, num_steps))
+            train_bar.set_description(
+                f"Epoch: {epoch + 1}/{num_epochs}, Loss: {loss_value}, Acc: {accuracy_value}, IoU:{IoU_value}")
 
-        if epoch % SHOW_EVERY == 0 or epoch == num_epochs - 1:
-            print(f"** Training\n\t\tLoss: {np.round(np.mean(training_loss_arr), 4)}\n\t\t"
-                  f"Accuracy: {np.round(np.mean(training_accuracy_arr), 4)}\n\t\t"
-                  f"IoU: {np.round(np.mean(training_IoU_arr), 4)}\n**\n\n")
+        training_loss_arr.append(np.mean(training_loss_batch))
+        training_accuracy_arr.append(np.mean(training_accuracy_batch))
+        training_IoU_arr.append(np.mean(training_IoU_batch))
 
         # Set loss (training) for each epoch
-        plot_train_loss[epoch] = train_loss_for_this_epoch
+        plot_train_loss.append(training_loss_arr[-1])
+
+        model.eval()
 
         # Validation
         with torch.no_grad():
 
-            num_steps = 0
-            validation_loss_batch = 0.0
-            validation_accuracy_batch = 0.0
-            validation_IoU_batch = 0.0
+            validation_loss_batch = []
+            validation_accuracy_batch = []
+            validation_IoU_batch = []
 
-            model.eval()
-
-            # Validation steps
-            bar = tqdm(validation_loader, total=len(validation_loader))
-            for X, y in bar:
-                torch.cuda.empty_cache()
-
+            # Validation loop
+            valid_bar = tqdm(validation_loader, total=len(validation_loader))
+            for X, y in valid_bar:
+                # Split the train data from the labels
                 X = X.to(device)
                 y = y.to(device)
                 y[y > 0] = 1
 
                 y_predicted = model(X)
                 loss = loss_fn(y_predicted, y)
-                validation_loss_batch += loss.item()
+                loss_value = loss.item()
 
-                validation_accuracy_batch += accuracy(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
-                validation_IoU_batch += IoU(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
+                accuracy_value = accuracy(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
 
-                num_steps += 1
+                IoU_value = IoU(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
 
-            valid_loss_for_this_epoch = np.divide(validation_loss_batch, num_steps)
-            valid_accuracy_for_this_epoch = np.divide(validation_accuracy_batch, num_steps)
-            valid_IoU_for_this_epoch = np.divide(validation_IoU_batch, num_steps)
+                # Added Loss
+                validation_loss_batch.append(loss_value)
 
-            validation_loss_arr.append(valid_loss_for_this_epoch)
-            validation_accuracy_arr.append(valid_accuracy_for_this_epoch)
-            validation_IoU_arr.append(valid_IoU_for_this_epoch)
+                # Added accuracy
+                validation_accuracy_batch.append(accuracy_value)
+
+                # Added IoU
+                validation_IoU_batch.append(IoU_value)
+
+                valid_bar.set_description(
+                    f"Epoch: {epoch + 1}, Loss: {loss_value}, Acc: {accuracy_value}, IoU:{IoU_value}")
+
+            validation_loss_arr.append(np.mean(validation_loss_batch))
+            validation_accuracy_arr.append(np.mean(validation_accuracy_batch))
+            validation_IoU_arr.append(np.mean(validation_IoU_batch))
 
             # Set loss and accuracy (validation) for each epoch
-            plot_validate_loss[epoch] = train_loss_for_this_epoch
-            plot_validate_accuracy[epoch] = valid_accuracy_for_this_epoch
-            plot_validate_IoU[epoch] = valid_IoU_for_this_epoch
+            plot_validate_loss.append(validation_loss_arr[-1])
+            plot_validate_accuracy.append(validation_accuracy_arr[-1])
+            plot_validate_IoU.append(validation_IoU_arr[-1])
 
-            if epoch % SHOW_EVERY == 0 or epoch == num_epochs - 1:
-                print(f"** Validation\n\t\tLoss: {np.round(np.mean(validation_loss_arr), 4)}\n\t\t"
-                      f"Accuracy: {np.round(np.mean(validation_accuracy_arr), 4)}\n\t\t"
-                      f"IoU: {np.round(np.mean(validation_IoU_arr), 4)}\n**\n\n")
+            print(
+                f"\n############### Train Loss: {training_loss_arr[-1]}, Train Acc: {training_accuracy_arr[-1]}, Train "
+                f"IOU: {training_IoU_arr[-1]} ###############")
+
+            print(
+                f"\n############### Valid Loss: {validation_loss_arr[-1]}, Valid Acc: {validation_accuracy_arr[-1]}, Valid "
+                f"IOU: {validation_IoU_arr[-1]} ###############")
 
         if epoch % 10 == 0:
             # Save model each n epochs
@@ -206,20 +193,18 @@ def test(test_loader, model, loss_fn):
     plot_mask = []
     plot_predicted = []
 
-    # Test
+    model.eval()
+
+    # Test loop
     with torch.no_grad():
-        model.eval()
-
         # Test steps
-        test_loss_batch = 0.0
-        test_accuracy_batch = 0.0
-        test_IoU_batch = 0.0
-        num_steps = 0
+        test_loss_batch = []
+        test_accuracy_batch = []
+        test_IoU_batch = []
 
-        bar = tqdm(test_loader, total=len(test_loader))
-        for X, y in bar:
-            torch.cuda.empty_cache()
-
+        test_bar = tqdm(test_loader, total=len(test_loader))
+        for X, y in test_bar:
+            # Split the train data from the labels
             X = X.to(device)
             y = y.to(device)
             y[y > 0] = 1
@@ -231,20 +216,26 @@ def test(test_loader, model, loss_fn):
             plot_predicted.append(y_predicted)
 
             loss = loss_fn(y_predicted, y)
-            test_loss_batch += loss.item()
+            loss_value = loss.item()
+            accuracy_value = accuracy(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
+            IoU_value = IoU(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
 
-            test_accuracy_batch += accuracy(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
-            test_IoU_batch += IoU(y_predicted.cpu().detach().numpy(), y.cpu().detach().numpy())
+            # Added Loss
+            test_loss_batch.append(loss_value)
 
-            num_steps += 1
+            # Added Accuracy
+            test_accuracy_batch.append(accuracy_value)
 
-        test_loss_arr.append(np.divide(test_loss_batch, num_steps))
-        test_accuracy_arr.append(np.divide(test_accuracy_batch, num_steps))
-        test_IoU_arr.append(np.divide(test_IoU_batch, num_steps))
+            # Added IoU
+            test_IoU_batch.append(IoU_value)
 
-        print(f"** Test\n\t\tLoss: {np.round(np.mean(test_loss_arr), 4)}\n\t\t"
-              f"Accuracy: {np.round(np.mean(test_accuracy_arr), 4)}\n\t\t"
-               f"IoU: {np.round(np.mean(test_IoU_arr), 4)}\n**\n\n")
+        test_loss_arr.append(np.mean(test_loss_batch))
+        test_accuracy_arr.append(np.mean(test_accuracy_batch))
+        test_IoU_arr.append(np.mean(test_IoU_batch))
+
+        print(
+            f"\n############### Test Loss: {test_loss_arr[-1]}, Test Acc: {test_accuracy_arr[-1]}, Test "
+            f"IOU: {test_IoU_arr[-1]} ###############")
 
     return plot_img, plot_mask, plot_predicted
 
@@ -268,10 +259,10 @@ def save_model(model, epoch, optimizer, training_loss, validation_loss, training
         'epoch': epoch,
         'state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
-        'Training Loss': np.round(np.mean(training_loss), 4),
-        'Validation Loss': np.round(np.mean(validation_loss), 4),
-        'Training Accuracy': np.round(np.mean(training_acc), 4),
-        'Validation Accuracy': np.round(np.mean(validation_acc), 4)
+        'Training Loss': np.mean(training_loss),
+        'Validation Loss': np.mean(validation_loss),
+        'Training Accuracy': np.mean(training_acc),
+        'Validation Accuracy': np.mean(validation_acc),
     }
 
     torch.save(checkpoint, PARENT_DIR + "/" + new_dir + "/" + name_file)
